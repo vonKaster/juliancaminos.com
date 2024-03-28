67d8031ed99a9e4893f6cab7528eee59360552fe
Fran <francocaminosmail@gmail.com>
2024-03-27 21:05:57 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,11 @@
-import { Download } from "lucide-react";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/Page/AnnotationLayer.css";
-import "react-pdf/dist/Page/TextLayer.css";
-import "./App.css";
-import { Button } from "./components/ui/button";
-
+import React, { useEffect } from 'react';
 
 function App() {
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    "pdfjs-dist/build/pdf.worker.min.js",
-    import.meta.url
-  ).toString();
+  useEffect(() => {
+    window.location.href = 'https://juliancaminos.com/cv.pdf';
+  }, []);
 
-  return (
-    <>
-    <div style={{width: '100%', backgroundColor: 'white', height: '40px'}}>
-    <a href="http://juliancaminos.com/cv.pdf" download="Julian Caminos - CV">
-
-    <Button style={{marginRight: '20px'}} variant='ghost'>
-          <Download className="mr-2 h-4 w-4" /> Descargar
-        </Button>
-        </a>
-    </div>
-    <div
-      style={{
-        height: "calc(100vh - 40px)",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#00314D",
-      }}
-      className="App"
-    >
-      
-      <div style={{ border: "4px solid white" }}>
-        <Document renderMode="canvas" file="http://juliancaminos.com/cv.pdf">
-          <Page scale={0.8} pageNumber={1} />
-        </Document>
-      </div>
-    </div>
-    </>
-  );
+  return null; // No renderizamos nada en la aplicación
 }
 
 export default App;
